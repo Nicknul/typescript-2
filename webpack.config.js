408b6052a2d0f2d1ce17dd9b9ad3ffdd67d7b5ca
Nicknul <jea7730@gmail.com>
2024-07-07 23:35:01 +0900

    edit_정호연 : 오타 수정

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './dist/index.js',
   output: {
-    path: path.relative(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.bundle.js',
   },
 };
